ec13082ca76162cf37c3b1bcbb4cf1127135dd80
Espen Myrland <espenm@met.no>
2018-08-06 13:59:16 +0200

    Added floydrun.sh script for running on floydhub.

---
+++ floydrun.sh
@@ -0,0 +1,1 @@
+floyd run   --gpu  --data metno/datasets/cclabels/1:/training_data  --env tensorflow-1.3  "python3 ./train.py --labelsfile alldata.txt --imagedir /training_data --outputdir output/v21"
