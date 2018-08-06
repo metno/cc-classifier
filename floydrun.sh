656a147693df9bd55726273768f75f6a1b3a884c
Espen Myrland <espenm@met.no>
2018-08-06 14:07:21 +0200

    tensorflow 1.3 -> 1.5

--- floydrun.sh
+++ floydrun.sh
@@ -1,1 +1,1 @@
-floyd run   --gpu  --data metno/datasets/cclabels/1:/training_data  --env tensorflow-1.3  "python3 ./train.py --labelsfile alldata.txt --imagedir /training_data --outputdir output/v21"
+floyd run   --gpu  --data metno/datasets/cclabels/1:/training_data  --env tensorflow-1.5  "python3 ./train.py --labelsfile alldata.txt --imagedir /training_data --outputdir output/v21"
