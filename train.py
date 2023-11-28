2e94d012a69711025c58ada73b5de50efab15b7e
Espen Myrland <espenm@met.no>
2023-11-28 15:53:59 +0100

    train2.py --> train.py . Use directory structure for training data instead of file with paths

--- train.py
+++ train.py
@@ -1,10 +1,17 @@
-''''exec python3 -u -- "$0" ${1+"$@"} # '''
-# Using this ^ to prevent stdout buffering running with nohup
+#!/usr/bin/env python3 
 
-import sys
-from matplotlib import pyplot
-#from keras.utils import to_categorical
-from tensorflow.keras.utils import to_categorical
+# Importing the models to try transfer learning on
+from tensorflow.keras.applications.resnet50 import ResNet50
+#from tensorflow.keras.applications.resnet50_v2 import ResNet50V2
+#from tensorflow.keras.applications.resnet50 import DenseNet201
+
+#from keras.applications.resnet50 import ResNet50
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import Model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
@@ -12,37 +19,100 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import LeakyReLU
-from tensorflow.keras.preprocessing.image import ImageDataGenerator 
 from tensorflow.keras.layers import BatchNormalization
-
-#from keras.optimizers import SGD
-from tensorflow.keras.optimizers import SGD
-import dataset
-import tensorflow as tf
-
+from sklearn.utils import compute_class_weight
+import numpy as np 
 import keras
-#from tensorflow import keras 
+from collections import Counter
+import tensorflow
 from keras.preprocessing import image
-import matplotlib.pyplot as plt
-import numpy as np
+# Setup some callbacs 
+cp_callback = tensorflow.keras.callbacks.ModelCheckpoint(
+                                                filepath="checkpoints/saved_model_{epoch:03d}.pb",
+                                                #filepath="checkpoints/saved_model_v1.pb",
+                                                save_weights_only=False,
+                                                verbose=0, save_freq='epoch',
+                                                monitor='val_accuracy',
+                                                save_best_only=True,
+                                                mode='max')
 
-validation_split = 0.25
-# load train and test dataset
-def load_dataset():
-    classes = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    # load dataset
-    trainX, trainY, testX, testY = dataset.read_train_sets2("/home/espenm/data/v53/alldata.txt",
-                                                                "/home/espenm/data/v53/training_data", 
-                                                                128, classes, validation_size=validation_split)
-    # one hot encode target values
-    trainY = to_categorical(trainY, 9, dtype='float32')
-    testY = to_categorical(testY, 9, dtype='float32')
-    return trainX, trainY, testX, testY
+tensorboard_callback = tensorflow.keras.callbacks.TensorBoard(log_dir="./tensorboard-data")
 
-# Define cnn model
+# This callback will stop the training when there is no improvement in
+# the validation loss for 100 consecutive epochs.
+early_stopping_callback = tensorflow.keras.callbacks.EarlyStopping(monitor='val_loss', patience=125, verbose=True, mode='auto')
+
+
+
+
+# */ // LATEST :-) !! */
+train_data_dir='/lustre/storeB/users/espenm/data/v2.0.3/train+validation/'
+img_size = 128
+batch_size = 128
+
+
+
+def preprocess(img):
+    width, height = img.shape[0], img.shape[1]
+    img = image.array_to_img(img, scale=False)
+
+    # Crop 48x48px
+    desired_width, desired_height = 48, 48
+
+    if width < desired_width:
+        desired_width = width
+    start_x = np.maximum(0, int((width-desired_width)/2))
+
+    img = img.crop((start_x, np.maximum(0, height-desired_height), start_x+desired_width, height))
+    img = img.resize((128, 128))
+
+    img = image.img_to_array(img)
+    return img / 255.
+
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    rotation_range=0.2,
+    horizontal_flip=True,
+    validation_split=0.2,
+    #preprocessing_function=preprocess
+    ) # set validation split
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle=True,
+    seed=42,
+    color_mode='rgb',
+    subset='training') # set as training data
+
+validation_generator = train_datagen.flow_from_directory(
+    train_data_dir, # same directory as training data
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle=True,
+    seed=42,
+    color_mode='rgb',
+    subset='validation') # set as validation data
+
+STEP_SIZE_TRAIN=train_generator.n // train_generator.batch_size
+STEP_SIZE_VALID=validation_generator.n // validation_generator.batch_size
+print("STEP_SIZE_TRAIN: %d, STEP_SIZE_VALID: %d" %(STEP_SIZE_TRAIN, STEP_SIZE_VALID))
+
+
+# Calculate class weights
+counter = Counter(train_generator.classes)
+max_val = float(max(counter.values()))
+train_class_weights = {class_id: max_val/num_images for class_id, num_images in counter.items()}
+
+
 def define_model():
 
-    # intput 128    
+   # intput 128    
     model = Sequential()
     model.add(Conv2D(128, (3, 3), activation='relu', padding='same', input_shape=(128, 128, 3)))
     model.add(BatchNormalization())
@@ -51,7 +121,7 @@ def define_model():
     model.add(MaxPooling2D((2, 2)))
     # This time it seems "Drop out" actually is the chance of 
     # "dropping out", and not the odds of "staying". Keras vs Tensorflow
-    model.add(Dropout(0.6))
+    model.add(Dropout(0.7))
     
     # 256 
     model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
@@ -83,88 +153,35 @@ def define_model():
     #model.add(Conv2D(2048, (3, 3), activation='relu', padding='same'))
     #model.add(BatchNormalization())
     #model.add(MaxPooling2D((2, 2)))
-    #model.add(Dropout(0.5))
+    #model.add(Dropout(0.7))
     
     # Output
     model.add(Flatten())
-    model.add(Dense(1024, activation='relu' ))
+    model.add(Dense(2048, activation='relu' ))
     model.add(BatchNormalization())
     model.add(Dropout(0.7))
     model.add(Dense(9, activation='softmax'))
     # compile model
-    opt = SGD(lr=0.001, momentum=0.9, learning_rate=1e-3)
+    opt = SGD( momentum=0.9, learning_rate=1e-3)
     #opt = tf.keras.optimizers.Adam(learning_rate=1e-3)
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-    [print(n.name) for n in tf.compat.v1.get_default_graph().as_graph_def().node]
+    #[print(n.name) for n in tf.compat.v1.get_default_graph().as_graph_def().node]
 
     model.summary()
     return model
 
-# plot diagnostic learning curves
-def summarize_diagnostics(history):
-    # plot loss
-    pyplot.subplot(211)
-    pyplot.title('Cross Entropy Loss')
-    pyplot.plot(history.history['loss'], color='blue', label='train')
-    pyplot.plot(history.history['val_loss'], color='orange', label='test')
-    # plot accuracy
-    pyplot.subplot(212)
-    pyplot.title('Classification Accuracy')
-    pyplot.plot(history.history['accuracy'], color='blue', label='train')
-    pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
-    # save plot to file
-    filename = sys.argv[0].split('/')[-1]
-    pyplot.savefig(filename + '_plot.png')
-    pyplot.close()
+model = define_model()
+# Train the last stage layers of ResNet50
+model.fit(
+        train_generator,
+        steps_per_epoch=STEP_SIZE_TRAIN,
+        epochs=1000,
+        validation_data=validation_generator,
+        validation_steps=STEP_SIZE_VALID, 
+        verbose=1,
+        class_weight=train_class_weights,
+        shuffle=True,
+        callbacks=[cp_callback, tensorboard_callback, early_stopping_callback]
+        
+        )
 
-# train model
-def train():
-    # Create a callback that saves the model + model's weights
-    cp_callback = tf.keras.callbacks.ModelCheckpoint(
-                                                filepath="checkpoints/saved_model_{epoch:03d}.pb",
-                                                #filepath="checkpoints/saved_model_v1.pb",
-                                                save_weights_only=False,
-                                                verbose=0, save_freq='epoch',
-                                                monitor='val_accuracy',
-                                                save_best_only=True,
-                                                mode='max')
-
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="/tmp/tf")
-
-
-    # This callback will stop the training when there is no improvement in
-    # the validation loss for 100 consecutive epochs.
-    early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=125, verbose=True, mode='auto')
-
-
-    # load dataset
-    trainX, trainY, testX, testY = load_dataset()
-    
-    # define model
-    model = define_model()
-    # fit model
-    
-    history = model.fit(trainX, trainY, epochs=1000, batch_size=128,
-                        validation_data=(testX, testY), verbose=1,
-                        callbacks=[cp_callback, tensorboard_callback, early_stopping_callback])
-
-
-    """
-    aug = ImageDataGenerator(rotation_range=0, width_shift_range=0.0, height_shift_range=0.0, shear_range=0.1,
-                                horizontal_flip=True)
-
-    # train the network
-    history = model.fit_generator(aug.flow(trainX, trainY, batch_size=64),
-            validation_data=(testX, testY), steps_per_epoch=len(trainX) // 64,
-            epochs=1000, callbacks=[cp_callback, tensorboard_callback], verbose=1)
-    """
-    # evaluate model
-    scores = model.evaluate(testX, testY, verbose=1)
-    print(model.metrics_names)
-    print(scores)
-    # learning curves
-    summarize_diagnostics(history)
-
-# entry point, train model
-if __name__ == "__main__":
-    train()
