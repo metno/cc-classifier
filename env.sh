f17a19b64d9c4d1ef9ef0ef0bc4230daa27ce2ab
Espen Myrland <espenm@met.no>
2023-11-29 12:52:00 +0000

    Updated Readme.md with keras model

--- env.sh
+++ env.sh
@@ -1,3 +1,8 @@
-export PROJECT_HOME_CNN=$(pwd)
-module load Python-devel/3.8.7-TF2.4.1
-#export PYTHONUSERBASE=/home/espenm/.local/py3.8.7
+source /modules/rhel8/conda/install/etc/profile.d/conda.sh
+conda activate TensorFlowGPU-03-2022
+
+#cd /lustre/storeB/users/espenm/cc-classifier
+#rm -rf checkpoints/* nohup.out /tmp/tf/*
+#nohup ./train.py &
+#sleep 2
+#tail -f nohup.out
