3782c7e33a307cbd0ad8f27f4357f269d3fd176c
Espen Myrland <espenm@met.no>
2024-01-17 12:50:51 +0100

    confusion_matrix.py fixes

--- support-scripts/confusion_matrix.py
+++ support-scripts/confusion_matrix.py
@@ -1,83 +1,39 @@
 #!/usr/bin/env python3
 
 import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 import argparse
 import re
 import numpy as np
-from pandas import *
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sn
 
+
 parser = argparse.ArgumentParser(description='Create a confusio namtrix')
-parser.add_argument('--predictions', type=str, help=" A file with lines containine path ccobs ccpred")
+parser.add_argument('--predictions', type=str, help="A file with lines containing \"^path answer(int) prediction(int)\"")
 args = parser.parse_args()
+dims = 3
 
-confusion_matrix = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0 ]
-]
-
-
+confusion_matrix = [[0] * dims for _ in range(dims)]
 count = 0
-
-cccnt = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 with open(args.predictions, "r") as ins:
     for line in ins:
-        myre = re.compile(r'(\S+)\s+(\d) (-?\d)$')
+        myre = re.compile(r'^(\S+)\s(\d)\s(-?\d)')
         mo = myre.search(line.strip())
         if mo is not None:
             image_file, cc, cc_cnn = mo.groups()
             cc = int(cc)
             cc_cnn = int(cc_cnn)
-            cccnt[cc] = cccnt[cc] + 1 
+          
         else:
-            print("ERror No Match")
+            print("ERror No Match1")
             continue
 
-        #if abs(cc_cnn - cc) <= 2:
-        #    confusion_matrix[cc][cc] = confusion_matrix[cc][cc] + 1
-        #else:
         confusion_matrix[cc_cnn][cc] = confusion_matrix[cc_cnn][cc] + 1
-#print(cccnt.index(min(cccnt)))
-#print(min(cccnt))
 
-cccnt2 = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
-count = 0
-with open(args.predictions, "r") as ins:
-    for line in ins:
-        myre = re.compile(r'(\S+)\s+(\d) (-?\d)$')
-        mo = myre.search(line.strip())
-        if mo is not None:
-            image_file, cc, cc_cnn = mo.groups()
-            cc = int(cc)
-            cc_cnn = int(cc_cnn)
-            cccnt2[cc] = cccnt2[cc] + 1
-            if cccnt2[cc] > min(cccnt):
-                continue
-            count = count + 1
-        else:
-            print("ERror No Match")
-            continue
 
-        #if abs(cc_cnn - cc) <= 2:
-        #    confusion_matrix[cc][cc] = confusion_matrix[cc][cc] + 1
-        #else:
-        #confusion_matrix[cc_cnn][cc] = confusion_matrix[cc_cnn][cc] + 1
-        #if cc_cnn != cc:
-        #    print("%d %d"  %(cc, cc_cnn))
-
-df_cm = DataFrame(confusion_matrix)
+df_cm = pd.DataFrame(confusion_matrix)
 print(df_cm)
 plt.figure(figsize = (10,7))
 title = 'Confusion matrix'
